e31c18940dc77d055e8f211d7812dc2b15b2ee38
Anay Gupta <777.anaycse@gmail.com>
2026-09-02 12:22:03 +0530

    feat : twilio-sarvam connection

--- whatsapp-service/app.py
+++ whatsapp-service/app.py
@@ -23,6 +23,49 @@ def whatsapp_webhook():
     num_media = int(request.form.get("NumMedia" , 0)) # gets me the number of media files in the message
     from_number = request.form.get("From") # gets me the phone number of the sender
 
-    resp = MessagingResponse()
+    resp = MessagingResponse() #
+
+
+
+    if num_media == 0:
+        resp.message("Please send a voice note describing your work and skills")
+        return str(resp)
+
+    media_url = request.form.get("MediaUrl0") # 
+    content_type = request.form.get("MediaContentType0") # gets me the content type of the media file
+
+    print(f"Received {content_type} from {from_number}: {media_url}")
+
+    local_input_path = os.path.join(AUDIO_DIR , "incoming.ogg")
+
+    download_twilio_media(media_url , local_input_path)
+
+    # Convert to a format Sarvam accepts i.e the wav format
+
+    wav_path = os.path.join(AUDIO_DIR , "incoming.wav")
+    convert_to_wav(local_input_path , wav_path)
+
+    transcript , detectedLanguage = sarvam_speech_to_text(wav_path) #  the audion is the input in sarvam supported format
+    print(f"Tranascript ({detectedLanguage}) : {transcript}")
+
+    # Reccomendation logic part
+    reply_text = get_recommendation_reply(transcript , from_number , detected_lang)
+
+    # Convert the reccomendation text to audio
+
+    reply_audio_filename = "reply.wav"
+    reply_audio_path = os.path.join(AUDIO_DIR , reply_audio_filename)
+
+    sarvam_text_to_speech(reply_text , detected_lang , reply_audio_path)
+
+    # Reply on WhatsApp with the voice note
+
+    reply_audio_public_url = f"{PUBLIC_BASE_URL}/audio/{reply_audio_filename}"
+
+    msg = resp.message(reply_text)
+
+    msg.media(reply_audio_public_url)
+
+    return str(resp)
 
     
